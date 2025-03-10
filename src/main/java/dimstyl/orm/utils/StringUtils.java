e79ac55e7026b2e3ad20348531688f6524ea53c3
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-03-10 02:04:56 +0200

    Create StringUtils.java

---
+++ src/main/java/dimstyl/orm/utils/StringUtils.java
@@ -0,0 +1,29 @@
+package dimstyl.orm.utils;
+
+public final class StringUtils {
+
+    private StringUtils() {
+    }
+
+    public static String getDefaultName(final String s) {
+        return StringUtils.camelCaseToSnakeCase(s);
+    }
+
+    public static String camelCaseToSnakeCase(String str) {
+        if (str == null || str.isBlank()) return str;
+
+        final StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            final char c = str.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                if (i > 0) result.append('_');
+                result.append(Character.toLowerCase(c));
+            } else result.append(c);
+        }
+
+        return result.toString();
+    }
+
+}
