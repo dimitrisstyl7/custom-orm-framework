c6e6c4d2b2f69d52fdedcfba4e7043e71ed26f8d
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-03-10 22:21:30 +0200

    Update DatabaseType.java
    - Delete `UNKNOWN` enum value.
    - Override `toString` method.

--- src/main/java/dimstyl/orm/enums/DatabaseType.java
+++ src/main/java/dimstyl/orm/enums/DatabaseType.java
@@ -2,6 +2,15 @@ package dimstyl.orm.enums;
 
 public enum DatabaseType {
 
-    H2, SQLITE, DERBY, UNKNOWN
+    H2, SQLITE, DERBY;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case H2 -> "H2";
+            case SQLITE -> "SQLite";
+            case DERBY -> "Derby";
+        };
+    }
 
 }
