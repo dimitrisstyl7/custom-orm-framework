8585ac117a4afe5a8f783fb797f4bc6d6d6f256e
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-03-10 13:13:38 +0200

    Create DerbyColumnTypeResolver.java

---
+++ src/main/java/dimstyl/orm/resolvers/DerbyColumnTypeResolver.java
@@ -0,0 +1,21 @@
+package dimstyl.orm.resolvers;
+
+import dimstyl.orm.exceptions.UnsupportedFieldTypeException;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+enum DerbyColumnTypeResolver implements ColumnTypeResolver {
+
+    INSTANCE;
+
+    // TODO: add link
+    private final Map<Class<?>, String> TYPE_MAP = Map.ofEntries(
+            // TODO: implement
+    );
+
+    @Override
+    public String resolve(Field field) throws UnsupportedFieldTypeException {
+        return ""; // TODO: implement
+    }
+}
