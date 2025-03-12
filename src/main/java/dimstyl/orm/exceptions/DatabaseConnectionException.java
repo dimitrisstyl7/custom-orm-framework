0f9ac702a271e1adfe05c8380f966690551da990
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-03-12 12:39:51 +0200

    Update DatabaseConnectionException.java
    - Add `DatabaseConnectionException(String message)` constructor.

--- src/main/java/dimstyl/orm/exceptions/DatabaseConnectionException.java
+++ src/main/java/dimstyl/orm/exceptions/DatabaseConnectionException.java
@@ -2,6 +2,10 @@ package dimstyl.orm.exceptions;
 
 public class DatabaseConnectionException extends RuntimeException {
 
+    public DatabaseConnectionException(String message) {
+        super(message);
+    }
+
     public DatabaseConnectionException(String message, Throwable cause) {
         super(message, cause);
     }
