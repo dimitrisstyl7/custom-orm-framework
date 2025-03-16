6f0a1b549c6689816b83d9b962319c1098bc4d1b
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-03-16 13:21:37 +0200

    Update SqlExecutionException.java

--- src/main/java/dimstyl/orm/exceptions/SqlExecutionException.java
+++ src/main/java/dimstyl/orm/exceptions/SqlExecutionException.java
@@ -2,6 +2,10 @@ package dimstyl.orm.exceptions;
 
 public class SqlExecutionException extends RuntimeException {
 
+    public SqlExecutionException(String message) {
+        super(message);
+    }
+
     public SqlExecutionException(String message, Throwable cause) {
         super(message, cause);
     }
